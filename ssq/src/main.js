f7446d1d9cb93f5683fa11a85a357ff17eb79811
Ryconler <1290279000@qq.com>
2019-07-25 19:23:58 +0800

    ssq

--- ssq/src/main.js
+++ ssq/src/main.js
@@ -1,23 +1,45 @@
-const {Crawl} = require('./Crawl')
 const cheerio = require('cheerio'),
+    request = require('request'),
     fs = require('fs'),
     path = require('path')
 
-let page = 1
-let flag = true
-const filename = path.resolve(__dirname, '..', 'data.txt')
+let flag = true  // 爬虫终止条件
+const filename = path.resolve(__dirname, '..', 'data.txt')  // 输出文件位置
 
-function writeFile(filename, data) {
-    fs.writeFile(filename, data, {flag: "a+"}, function (err) {
-        if (err) throw err;
+function getHTML(url, callback) {
+    const options = {
+        url: url,
+        // 设置一个浏览器头
+        headers: {
+            "Accept": "application/json, text/javascript, */*; q=0.01",
+            "X-Requested-With": "XMLHttpRequest",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+        }
+    }
+    request(options, function (err, res) {
+        if (!err) {
+            callback && callback(res.body)
+        } else {
+            console.log(err);
+            flag = false
+            callback && callback('')
+        }
     })
 }
 
-function getData(html) {
+function writeFile(filename, data) {
+    fs.writeFile(filename, data, {flag: "a+"}, err => {
+            throw err
+        }
+    )
+}
+
+function handle(html) {
     try {
         const $ = cheerio.load(html)
         const tr = $('tr')
-        if (tr.find('td').length===0) {  // 爬取结束
+        if (tr.find('td').length === 0) {  // 爬取结束
             flag = false
             return
         }
@@ -43,15 +65,15 @@ function getData(html) {
     }
 }
 
-const crawl = new Crawl()
+let page = 1
 
 let t = setInterval(function () {
     if (flag) {
         console.log('开始爬取第' + page + '页');
-        crawl.getHTML( `http://kaijiang.zhcw.com/zhcw/html/ssq/list_${page}.html`, getData)
-        page ++
+        getHTML(`http://kaijiang.zhcw.com/zhcw/html/ssq/list_${page}.html`, handle)
+        page++
     } else {
         console.log('爬取结束');
         clearInterval(t)
     }
-}, 500)
+}, 500)  // 间隔0.5s爬取一次
